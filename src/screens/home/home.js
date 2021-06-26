b748ce92ed6454e8da40955527f8c8bf77b8d7e6
Vinicius Dias Correa de Assis <vinicius.dcassis@gmail.com>
2021-06-26 11:43:15 -0300

    teste

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -4,6 +4,7 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>NOTICIAS WEB</h1>
         <input></input>
         <button style={{width: 200, height: 40}}>Botao</button>
       </header>
