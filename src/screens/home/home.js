757c5e89526ca8d80e958d407d00be44cc210f45
Vinicius Dias Correa de Assis <vinicius.dcassis@gmail.com>
2021-06-26 11:11:01 -0300

    atualizacao

---
+++ src/screens/home/home.js
@@ -0,0 +1,14 @@
+import './home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input></input>
+        <button style={{width: 200, height: 40}}>Botao</button>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
